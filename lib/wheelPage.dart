06d1888af3271516ca04fa728140044344b99242
thetechgirlgita <thetechgirlgita@gmail.com>
2023-02-16 17:03:09 +0530

    Deletinbg and setup

---
+++ lib/wheelPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class wheeler extends StatefulWidget {
+  const wheeler({super.key});
+
+  @override
+  State<wheeler> createState() => _wheelerState();
+}
+
+class _wheelerState extends State<wheeler> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
