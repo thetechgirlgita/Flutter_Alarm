b5528f0ac3df13b777ed4404cc03ec97c860a7f9
thetechgirlgita <thetechgirlgita@gmail.com>
2023-02-16 23:53:31 +0530

    Adding spin agin

--- lib/spinWheel.dart
+++ lib/spinWheel.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'wheelPage.dart';
@@ -12,18 +10,46 @@ class spinWheel extends StatefulWidget {
 }
 
 class _spinWheelState extends State<spinWheel> {
+  int selected = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height / 3,
-        width: MediaQuery.of(context).size.width / 2,
-        child: FortuneWheel(animateFirst: false, items: [
-          FortuneItem(child: Text("1000k")),
-          FortuneItem(child: Text("10k")),
-          FortuneItem(child: Text("100")),
-        ]),
-      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width / 2,
+              child: FortuneWheel(
+                //  selected: selected,
+                animateFirst: true,
+                items: [
+                  FortuneItem(
+                    child: Text("1000k"),
+                  ),
+                  FortuneItem(child: Text("10k")),
+                  FortuneItem(child: Text("100")),
+                  FortuneItem(child: Text("10")),
+                  FortuneItem(child: Text("600k")),
+                  FortuneItem(child: Text("18k")),
+                  FortuneItem(child: Text("300")),
+                  FortuneItem(child: Text("178")),
+                ],
+              ),
+            ),
+            GestureDetector(
+                child: TextButton(
+                    child: Text('Spin Again'),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => spinWheel(),
+                          ));
+                    }))
+          ])),
     );
   }
 }
